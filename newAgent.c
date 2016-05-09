45146b1a34cb6822f83004c5c5e2f6e47adcac65
Param-S <sparameswara@gmail.com>
2016-05-09 16:38:54 +0530

    a new agent trace the long pause time & take the configured action. -agentlib/-agentpath:<agentname>=pausetime=xyz,command=cmd

--- newAgent.c
+++ newAgent.c
@@ -4,68 +4,44 @@
 #include <jni.h>
 #include <jvmti.h>
 
-// socket includes
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
 #include <errno.h>
 #include <sys/types.h>
+#include <pthread.h>
+#include <time.h>
+
 
 /* Global static data */
 static jvmtiEnv *jvmti;
-static long int gc_count;
+static long int gc_count = 0;
 static jrawMonitorID lock;
 int thread_started = 0;
-int client_connected = 0;
-int listenfd = 0;
-int connfd = 0;
+int ptime = 4;
+char cmd[1024]={'l','s','\0'};
 
-
-// socket initialization
-int socket_init(void) 
-{
-    struct sockaddr_in serv_addr;
-    int numrv;  
-
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    memset(&serv_addr, '0', sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;    
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); 
-    serv_addr.sin_port = htons(5250);    
-
-    bind(listenfd, (struct sockaddr*)&serv_addr,sizeof(serv_addr));
-    if(listen(listenfd, 10) == -1){
-        printf("Failed to listen\n");
-        return -1;
-    }
-
-    return 0;
-}
+pthread_mutex_t agnt_lock;
+pthread_cond_t agnt_cvar;
 
 static void JNICALL
-socket_listener(jvmtiEnv* jvmti, JNIEnv* jni, void *p) 
+gc_listener(jvmtiEnv* jvmti, JNIEnv* jni, void *p)
 {
-    /* loop until successfully connected to a client */
-    fprintf(stderr, "socket listener waiting for clients\n");
-    while ((connfd = accept(listenfd, (struct sockaddr*)NULL ,NULL)) == -1) ;
-    client_connected = 1;
-    fprintf(stderr, "Client connected.. Hurray!!!\n");
-}
-
-// gc_state 1 => gc start, 0 => gc end
-void send_message(gc_state) 
-{
-    char sendBuff[1025];
-    memset(sendBuff, '0', sizeof(sendBuff));
-
-    if(gc_state)
-	sprintf(sendBuff, "GCStart:%ld", gc_count);
-    else {
-	sprintf(sendBuff, "GCEnd:%ld", gc_count);
-    }
-    write(connfd, sendBuff, strlen(sendBuff));
+    struct timespec wait_time;
+    int pid;
+    pthread_mutex_lock(&agnt_lock);
+    do {
+        if (gc_count == 1) {
+	    clock_gettime(CLOCK_REALTIME, &wait_time);
+	    wait_time.tv_sec += ptime;
+	    if (pthread_cond_timedwait(&agnt_cvar, &agnt_lock, &wait_time) == ETIMEDOUT && gc_count == 1) {
+		/* take defined action */	
+		fprintf(stderr, "gc pause time exceeded defined value\n");
+		pid = fork();
+		if (pid == 0) {
+		    execvp(cmd, NULL);
+		}
+	    } 
+	} 
+        pthread_cond_wait(&agnt_cvar, &agnt_lock);
+    } while (1);
 }
 
 /* Creates a new jthread */
@@ -90,17 +66,11 @@ vm_init(jvmtiEnv *jvmti, JNIEnv *env, jthread thread) {
     jvmtiError err;
     int ret;
 
-    fprintf(stderr, "VMInit...\n");
-    ret = socket_init();
-    if(ret != 0) {
-        fprintf(stderr, "WARNING:  Socket Failed to initialize, err=%d", ret);
+    err = (*jvmti)->RunAgentThread(jvmti, alloc_thread(env), &gc_listener, NULL, JVMTI_THREAD_NORM_PRIORITY);
+    if (err != JVMTI_ERROR_NONE) {
+       fprintf(stderr, "ERROR: failed to create gc agent thread, err=%d\n", err);
     } else {
-    	err = (*jvmti)->RunAgentThread(jvmti, alloc_thread(env), &socket_listener, NULL, JVMTI_THREAD_NORM_PRIORITY);
-    	if (err != JVMTI_ERROR_NONE) {
-            fprintf(stderr, "ERROR: Socket Listener failed, err=%d\n", err);
-    	} else {
-            thread_started = 1;
-	}
+	thread_started = (pthread_mutex_init(&agnt_lock, NULL) == 0)?(pthread_cond_init(&agnt_cvar, NULL) == 0)?1:0:0;
     }
 }
 
@@ -110,11 +80,11 @@ gc_start(jvmtiEnv* jvmti)
 {
     jvmtiError err;
     fprintf(stderr, "GarbageCollectionStart...\n");
-    if(thread_started && client_connected) {
-    	err = (*jvmti)->RawMonitorEnter(jvmti, lock);
-        gc_count++;
-        err = (*jvmti)->RawMonitorExit(jvmti, lock);
-        send_message(1);
+    if(thread_started) {
+	pthread_mutex_lock(&agnt_lock);
+        gc_count = 1;
+	pthread_cond_signal(&agnt_cvar);
+	pthread_mutex_unlock(&agnt_lock);
     }
     
 }
@@ -125,8 +95,11 @@ gc_finish(jvmtiEnv* jvmti_env)
 {
     jvmtiError err;
     fprintf(stderr, "GarbageCollectionFinish...\n");
-    if(thread_started && client_connected) {
-        send_message(0);
+    if(thread_started) {
+	pthread_mutex_lock(&agnt_lock);
+	gc_count = 0;
+	pthread_cond_signal(&agnt_cvar);
+	pthread_mutex_unlock(&agnt_lock);
     }
 }
 
@@ -188,21 +161,16 @@ Agent_OnLoad(JavaVM *vm, char *options, void *reserved) {
         return -1;
     }
 
-    /* Create the necessary raw monitor */
-    err = (*jvmti)->CreateRawMonitor(jvmti, "lock", &lock);
-    if (err != JVMTI_ERROR_NONE) {
-        fprintf(stderr, "ERROR: Unable to create raw monitor: %d\n", err);
-        return -1;
-    }
+    /* parse the agent options & set corresponding variables */
+    sscanf(options, "pausetime=%d,command=%s", &ptime, cmd);    
     return 0;
-
 }
 
 /* Agent_OnUnload() is called last */
 JNIEXPORT void JNICALL
 Agent_OnUnload(JavaVM *vm)
 {
-    if(thread_started && client_connected) {
-    	close(connfd);
+    if(thread_started) {
+	/* cleanup required */
     }
 }
